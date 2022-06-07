a5b487c08526bcc64ff4504a60d24d30ab2d3144
Gabriel L <gabriellovato15@gmail.com>
2022-06-07 15:44:47 -0300

    Cards no início reformulados

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,19 +16,20 @@
 
     <script src="js/carrossel.js" defer></script>
 </head>
+
 <body>
     <!-- Import Components -->
     <?php include_once("templates/header.php"); ?>
     <?php include_once("templates/footer.php"); ?>
     <!--  -->
-    
+
     <?php echo get_header(); ?>
     <section class="carrossel">
         <img src="public/images/keyboard.webp" alt="">
         <img src="public/images/keyboard2.jpg" alt="">
         <img src="public/images/keyboard3.webp" alt="">
         <img src="public/images/keyboard4.webp" alt="">
-        <h1 class="logo"></h1>
+        <h1 class="force-white logo"></h1>
         <h1>
             Teclados <br>
             Por Quem <br>
@@ -40,36 +42,52 @@
     <main>
         <h1>Compre nossos produtos e etc.</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium quae delectus harum. Nihil reiciendis quibusdam vero, sequi doloremque omnis laboriosam amet modi! Error ad assumenda impedit in nihil velit sint.
-        Ut sit pariatur reprehenderit. Incidunt officiis fugit assumenda omnis dolore deleniti neque, ipsam sint a ea vitae iusto velit nisi distinctio culpa ut doloribus exercitationem provident illum facere ex? Fugit.</p>
+            Ut sit pariatur reprehenderit. Incidunt officiis fugit assumenda omnis dolore deleniti neque, ipsam sint a ea vitae iusto velit nisi distinctio culpa ut doloribus exercitationem provident illum facere ex? Fugit.</p>
     </main>
-    
+
     <section class="list">
         <div>
-            <img src="public/images/keyboard.webp" alt="">
+            <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
+                <path d="M19.2 36.4 14.45 25.95 4 21.2 14.45 16.45 19.2 6 23.95 16.45 34.4 21.2 23.95 25.95ZM36.4 42 34.05 36.75 28.8 34.4 34.05 32 36.4 26.8 38.8 32 44 34.4 38.8 36.75Z" />
+            </svg>
+            <!-- <img src="public/images/keyboard.webp" alt=""> -->
             <span>
-                <h1>Bunda</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, expedita veniam non officia deserunt doloribus? Consequatur, ipsum libero quibusdam sint fuga voluptas ullam illo id provident magni molestiae, dolorum exercitationem.</p>
+                <h1>Qualidade</h1>
+                <p>Componentes de qualidade que não te deixarão na mão</p>
+                <!-- <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, expedita veniam non officia deserunt doloribus? Consequatur, ipsum libero quibusdam sint fuga voluptas ullam illo id provident magni molestiae, dolorum exercitationem.</p> -->
             </span>
         </div>
         <div>
-            <img src="public/images/keyboard.webp" alt="">
+            <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
+                <path d="M17.9 16.75 23.15 6H24.85L30.1 16.75ZM22.75 40.5 5.05 19.25H22.75ZM25.25 40.5V19.25H42.95ZM32.9 16.75 27.65 6H38L43.35 16.75ZM4.65 16.75 10 6H20.35L15.1 16.75Z" />
+            </svg>
+            <!-- <img src="public/images/keyboard.webp" alt=""> -->
             <span>
-                <h1>Bunda</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, expedita veniam non officia deserunt doloribus? Consequatur, ipsum libero quibusdam sint fuga voluptas ullam illo id provident magni molestiae, dolorum exercitationem.</p>
+                <h1>Durabilidade</h1>
+                <p>Peças fabricadas e analisadas individualmente, durabilidade de mais de 30 milhões de cliques</p>
+                <!-- <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, expedita veniam non officia deserunt doloribus? Consequatur, ipsum libero quibusdam sint fuga voluptas ullam illo id provident magni molestiae, dolorum exercitationem.</p> -->
             </span>
         </div>
         <div>
-            <img src="public/images/keyboard.webp" alt="">
+            <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
+                <path d="M18.6 43.6Q18.15 44.05 17.525 44.025Q16.9 44 16.45 43.55Q16.05 43.15 16 42.55Q15.95 41.95 16.3 41.5L26 28L9.8 26.45Q8.5 26.35 8.075 25.1Q7.65 23.85 8.65 23L29.4 4.4Q29.85 3.95 30.475 3.975Q31.1 4 31.55 4.45Q31.95 4.85 32 5.45Q32.05 6.05 31.7 6.5L22 20L38.2 21.55Q39.5 21.65 39.925 22.9Q40.35 24.15 39.35 25Z" />
+            </svg>
+            <!-- <img src="public/images/keyboard.webp" alt=""> -->
             <span>
-                <h1>Bunda</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, expedita veniam non officia deserunt doloribus? Consequatur, ipsum libero quibusdam sint fuga voluptas ullam illo id provident magni molestiae, dolorum exercitationem.</p>
+                <h1>Agilidade</h1>
+                <p>Circuitos modernos e precisos, tempos de resposta com menos de 3ms de latência</p>
+                <!-- <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, expedita veniam non officia deserunt doloribus? Consequatur, ipsum libero quibusdam sint fuga voluptas ullam illo id provident magni molestiae, dolorum exercitationem.</p> -->
             </span>
         </div>
         <div>
-            <img src="public/images/keyboard.webp" alt="">
+            <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
+                <path d="M17.3 45 13.5 38.5 5.95 36.95 6.8 29.6 2 24 6.8 18.45 5.95 11.1 13.5 9.55 17.3 3 24 6.1 30.7 3 34.55 9.55 42.05 11.1 41.2 18.45 46 24 41.2 29.6 42.05 36.95 34.55 38.5 30.7 45 24 41.9ZM21.85 30.65 33.2 19.4 30.95 17.35 21.85 26.35 17.1 21.4 14.8 23.65Z" />
+            </svg>
+            <!-- <img src="public/images/keyboard.webp" alt=""> -->
             <span>
-                <h1>Bunda</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, expedita veniam non officia deserunt doloribus? Consequatur, ipsum libero quibusdam sint fuga voluptas ullam illo id provident magni molestiae, dolorum exercitationem.</p>
+                <h1>Confiabilidade</h1>
+                <p>Seguro, garantia de entrega e assistência técnica individual e feita por humanos</p>
+                <!-- <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, expedita veniam non officia deserunt doloribus? Consequatur, ipsum libero quibusdam sint fuga voluptas ullam illo id provident magni molestiae, dolorum exercitationem.</p> -->
             </span>
         </div>
     </section>
@@ -77,9 +95,12 @@
         <h1>Ainda não sabe <br> se quer um <br> teclado bom?</h1>
         <a class="no-animation" href="teste.php">
             <span>Experimente</span>
-            <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M24 40 21.9 37.85 34.25 25.5H8V22.5H34.25L21.9 10.15L24 8L40 24Z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48">
+                <path d="M24 40 21.9 37.85 34.25 25.5H8V22.5H34.25L21.9 10.15L24 8L40 24Z" />
+            </svg>
         </a>
     </div>
     <?php echo get_footer() ?>
 </body>
+
 </html>
