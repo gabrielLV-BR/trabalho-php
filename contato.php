ca60a9af184bb303958306510271a80b730c58ec
Gabriel L <gabriellovato15@gmail.com>
2022-06-07 09:55:13 -0300

    Terminada página contato

--- contato.php
+++ contato.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,33 +8,81 @@
 
     <link rel="stylesheet" href="public/css/global.css">
     <link rel="stylesheet" href="public/css/header.css">
+    <link rel="stylesheet" href="public/css/footer.css">
     <link rel="stylesheet" href="public/css/contato.css">
 
     <title>Teck | Teclados por quem sabe o que faz</title>
 </head>
+
 <body>
-    <?php include_once("templates/header.php"); ?>
+    <?php 
+    include_once("templates/header.php"); 
+    include_once("templates/footer.php"); 
+    ?>
     <?php echo get_header(); ?>
 
-    <div id="formulario">
+    <main>
+        <h1>Formulário de Contato</h1>
         <form action="">
-           <div class="blocoForm">
-               <p>Nome</p>
-               <input type="text" placeholder="Digite seu nome" required>
-           </div>
-           <div class="blocoForm">
-               <p>Nascimento</p>
-               <input type="date" required>
-           </div>
-           <div class="blocoForm">
-               <p>E-mail</p>
-               <input type="email" placeholder="nome@mail.com" required>
-           </div>
-           <div class="blocoForm">
-               <p>Telefone</p>
-               <input type="num" placeholder="(99) 9 9999-9999" maxlength="11" required>
-           </div>
+            <section>
+                <label>
+                    <span>Nome</span>
+                    <input name="nome" type="text" placeholder="Digite seu nome" required>
+                </label>
+                <label>
+                    <span>Nascimento</span>
+                    <input name="nascimento" type="date" required>
+                </label>
+            </section>
+            <section>
+
+                <label>
+                    <span>E-mail</span>
+                    <input name="email" type="email" placeholder="nome@mail.com" required>
+                </label>
+                <label>
+                    <span>Telefone</span>
+                    <input name="numero" type="" placeholder="(99) 9 9999-9999" maxlength="11" required>
+                </label>
+            </section>
+            <section>
+                <label>
+                    <span>Endereço</span>
+                    <input name="endereco" type="text" placeholder="Endereço" required>
+                </label>
+            </section>
+            <section>
+                <label>
+                    <span>Cidade</span>
+                    <input name="cidade" type="text" placeholder="Cidade" required>
+                </label>
+                <label>
+                    <span>Estado</span>
+                    <input name="estado" type="text" placeholder="Estado" required>
+                </label>
+            </section>
+            <hr>
+            <label>
+                <span>Assunto</span>
+                <select name="assunto" id="" required>
+                    <option value="" disabled selected>Selecione...</option>
+                    <option value="Sugestão">Sugestão</option>
+                    <option value="Reclamação">Reclamação ou Elogio</option>
+                    <option value="Reembolso">Reembolso</option>
+                    <option value="Privacidade">Políticas de privacidade</option>
+                    <option value="Legal">Contato legal</option>
+                    <option value="Outro">Outro...</option>
+                </select>
+            </label>
+            <label>
+                <span>Mensagem</span>
+                <textarea name="mensagem" cols="30" rows="10"></textarea>
+            </label>
+            <input class="cool-hover" type="submit" value="Enviar">
         </form>
-    </div>
+    </main>
+
+    <?php echo get_footer() ?>
 </body>
+
 </html>
